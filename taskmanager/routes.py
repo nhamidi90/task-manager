1a2fed3101ce59cb986a0f72cc7b176f065a541b
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-04-12 14:01:23 +0000

    Add functionality for displaying category cards

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,7 +10,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=['GET', 'POST'])
@@ -19,5 +20,5 @@ def add_category():
         category = Category(category_name = request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
-        return redirect(url_for"categories")
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
