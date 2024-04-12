4953a7b9bebd4165791a6c3de85b2fecf16d7b37
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-04-12 12:28:07 +0000

    Create database

---
+++ taskmanager/models.py
@@ -0,0 +1,34 @@
+from taskmanager import db
+
+
+
+class Category(db.Model):
+    # schema for the catgory model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+    
+
+
+class Task(db.Model):
+    # schema for the catgory model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    # ondelete="CASCADE" means if one category is deleted it will delete any tasks linked to it
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
+        # Can also use f"{strings}":
+        # return f"#{self.id} - Task {self.task_name} | Urgent: {self.is_urgent}"
