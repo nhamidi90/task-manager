f600e7b7beced9add19a2bf5246f4da576839309
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-04-15 13:14:05 +0000

    Add code to allow users to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    //select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
