c7b6d20fe28650c17c6746d33776d9da0ae84668
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-04-16 10:12:19 +0000

    Fix typo

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -3,7 +3,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for the catgory model
+    # schema for the category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
@@ -27,7 +27,7 @@ class Task(db.Model):
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return "#{0} - Task {1} | Urgent: {2}".format(
+        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
         # Can also use f"{strings}":
